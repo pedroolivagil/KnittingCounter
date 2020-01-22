8fdc553688756b83d667d4c125318af80216b07d
Pedro Oliva <onion_oliva@hotmail.com>
2020-01-22 05:20:55 +0100

    v.0.15 SplashScreen

--- app/src/main/java/com/olivadevelop/knittingcounter/SplashscreenActivity.java
+++ app/src/main/java/com/olivadevelop/knittingcounter/SplashscreenActivity.java
@@ -1,20 +1,48 @@
 package com.olivadevelop.knittingcounter;
 
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashscreenActivity extends AppCompatActivity {
 
     protected boolean active = true;
     protected float segundos = 2.5f;
     protected float splashTime = segundos * 1000;
+    private boolean mVisible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splashscreen);
+        View decorView = getWindow().getDecorView();
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+        loadMainActivity();
+    }
+
+    private void loadMainActivity() {
         Thread splashThread = new Thread() {
             @Override
             public void run() {
@@ -35,5 +63,29 @@ public class SplashscreenActivity extends AppCompatActivity {
             }
         };
         splashThread.start();
+    }
+
+//---------------------------------------------------------------
+
+    private void toggle() {
+        if (mVisible) {
+            hide();
+        } else {
+            show();
+        }
+    }
+
+    private void hide() {
+        // Hide UI first
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        mVisible = false;
+    }
+
+    @SuppressLint("InlinedApi")
+    private void show() {
+        mVisible = true;
     }
 }
