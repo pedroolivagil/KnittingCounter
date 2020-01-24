961073f3f454a0eb2b39abd9554808b48312bb50
0013856 <20081991_Aa>
2020-01-24 14:42:10 +0100

    v.0.32

--- app/src/main/java/com/olivadevelop/knittingcounter/MainActivity.java
+++ app/src/main/java/com/olivadevelop/knittingcounter/MainActivity.java
@@ -53,6 +53,8 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        this.drawer.openDrawer(Gravity.LEFT);
     }
 
     @Override
